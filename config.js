0957da3963e5383047832af82efc1cc04248309c
tuilad01 <thelegendaglx@gmail.com>
2019-08-13 15:24:08 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,10 @@
+const config = {
+    apiUrl: "",
+    localStorage: {
+        learn: "a6bffeba-bda0-11e9-9cb5-2a2ae2dbcce4",
+        groups: "b81d9898-bda0-11e9-9cb5-2a2ae2dbcce4",
+        words: "a68618ca-bda1-11e9-9cb5-2a2ae2dbcce4"
+    }
+}
+
+export default config;
